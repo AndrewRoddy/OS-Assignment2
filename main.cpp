7cd7675d8e263fdb405454447fb13fd2e9af3823
AndrewRoddy <andrewroddy7@gmail.com>
2025-11-21 18:45:09 -0500

    Parses and closes file

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using std::vector;
 
@@ -9,12 +10,11 @@ class Banker {
 public:
     Banker(std::string file) { 
         openFile(file); // Opens the file
-        // parseFile();    // Gets the variables
-        // closeFile();
+        parseFile();    // Gets the variables
+        closeFile();    // Closes the file
      }
 
-    void openFile(const std::string& fileName);
-    // void parseFile();
+    void print() const;
 
     // Getters
     std::ifstream& getFile() { return file_; };
@@ -23,6 +23,10 @@ public:
     vector<int>         getAvailable()  const { return available_; }
 
 private:
+    void openFile(const std::string& fileName);
+    void parseFile();
+    void closeFile();
+
     vector<vector<int>> allocation_;
     vector<vector<int>> max_;
     vector<int> available_;
@@ -43,17 +47,63 @@ void Banker::openFile(const std::string& fileName) {
     }
     std::cout << "\033[37m"; // Reset text color back to white
 }
+void Banker::closeFile() {
+    if (file_.is_open()) {
+        file_.close();
+        std::cout << "\033[32m"; // Makes th text green
+        std::cout << "File closed!" << std::endl;
+        std::cout << "\033[37m"; // Reset text color back to white
+    }
+}
 
-// void Banker::parseFile();
+void Banker::parseFile() {
+
+    // Parse Mode
+    enum class PM { none, allocation, max, available };
+    PM mode = PM::none;
+
+    std::string line;
+    while (std::getline(file_, line)) {
+        if (line.empty()) continue;
+
+        // Change the mode if necessary
+        if (line.find("---") != std::string::npos) { 
+
+            // Sets the mode based on line
+            if (line.find("allocation") != std::string::npos) {
+                mode = PM::allocation;
+            } else if (line.find("max") != std::string::npos) {
+                mode = PM::max;
+            } else if (line.find("available") != std::string::npos) {
+                mode = PM::available;
+            }
+        } else {
+            // Sets the vector based on the mode
+
+            // Makes the vector
+            // Subtracts 48 to make it a proper number
+            vector<int> vec = {
+                line[0] - 48,
+                line[3] - 48,
+                line[6] - 48
+            };
+
+            // Based on the mode add to the vector
+            if (mode == PM::allocation) allocation_.push_back(vec);
+            if (mode == PM::max)        max_.push_back(vec);
+            if (mode == PM::available)  available_ = vec;
+        }
+    }
+}
+
+// void Banker::print() const {
+
+// }
 
 int main() {
     
     // Opens file as the bank on run
     Banker bank("input-file.txt");
-    std::ifstream& file = bank.getFile();
-    
-    std::string line;
-    while (std::getline(file, line)) {
-        std::cout << line << std::endl;
-    }
+    // bank.print();
+
 }
