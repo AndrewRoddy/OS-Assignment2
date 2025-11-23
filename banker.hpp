e8f5a7e5abf90bbc661341bd5e5481f278c27a2d
Andrew Roddy <andrewroddy7@gmail.com>
2025-11-22 20:03:59 -0500

    Seperated into multiple files

---
+++ banker.hpp
@@ -0,0 +1,49 @@
+
+#ifndef BANKER_HPP_
+#define BANKER_HPP_
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using std::vector;
+
+class Banker {
+public:
+    Banker(std::string file) { 
+        openFile(file); // Opens the file
+        parseFile();    // Gets the variables
+        getNeed();      // Finds the need
+        safeState_ ={}; // Makes sure safe state is empty
+        closeFile();    // Closes the file
+    }
+
+    void print() const;
+    bool bankersAlgorithm();
+
+    void openFile(const std::string& fileName);
+    void parseFile();
+    void getNeed();
+    void closeFile();
+    
+    bool allocatable(int, vector<int>&) const;
+    vector<int> getSafeState() {
+        if (safeState_.empty()) { bankersAlgorithm(); }
+        return safeState_;
+    }
+
+private:
+
+    vector<vector<int>> allocation_;
+    vector<vector<int>> max_;
+    vector<vector<int>> need_;
+
+    vector<int> available_;
+    vector<int> safeState_;
+
+    std::ifstream file_;
+};
+
+#endif
